08c7425f311e281c3624fd332d63a119bc284046
jan.christopher.vogt <jan.christopher.vogt@c1c940d7-3282-706a-30e0-39f11b4449dc>
2010-06-24 22:27:47 +0000

    made example more readable

--- de/funcy/unittests/Run.java
+++ de/funcy/unittests/Run.java
@@ -4,48 +4,46 @@ import de.funcy.*;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public class Run {
 
 	public static void main(String[] args) {
-		Collection<String> list = Arrays.asList("1", "2", "3", "4", "5", "6",
-				"7");
-		System.out.println(list);
+		List<String> list = Arrays.asList("1", "2", "3", "4", "5", "6", "7");
+		System.out.println("List of Strings:   " + list);
 
 		// map: String to Integer
-		Collection<Integer> l3 = new Mapping<String, Integer>() {
+		Collection<Integer> list2 = new Mapping<String, Integer>() {
 			public Integer function(String s) {
 				return Integer.valueOf(s);
 			}
 		}.apply(list);
-		System.out.println(l3);
+		System.out.println("List of Integers:  " + list2);
 
 		// filter: even numbers
-		Collection<Integer> l4 = new Filter<Integer>() {
+		Collection<Integer> list3 = new Filter<Integer>() {
 			public Boolean function(Integer s) {
 				return s % 2 == 0;
 			}
-		}.apply(l3);
-
-		System.out.println(l4);
+		}.apply(list2);
+		System.out.println("Filtered even:     " + list3);
 
 		// map with closure: divide every value by 2
 		final int divisor = 2;
-		Collection<Integer> l5 = new Mapping<Integer, Integer>() {
+		Collection<Integer> list4 = new Mapping<Integer, Integer>() {
 			public Integer function(Integer i) {
 				return i / divisor;
 			}
-		}.apply(l4);
-
-		System.out.println(l5);
+		}.apply(list3);
+		System.out.println("All divded by two: " + list4);
 
 		// reduce: compute the sum
 		Integer sum = new Reduction<Integer>() {
 			public Integer function(Integer a, Integer b) {
 				return a + b;
 			}
-		}.apply(l5);
-		System.out.println(sum);
+		}.apply(list4);
+		System.out.println("Sum: " + sum);
 
 		// chaining map and filter with final reduce (not type safe): do all at
 		// once
